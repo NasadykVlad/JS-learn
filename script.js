c0a265423fe10bf9cd3576696c7c557ff2ef91c2
Nasadyk Vladyslav <t.empire228@gmail.com>
2020-03-25 16:04:42 +0200

    2. Osnovy

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert ('Vlad');
+"use strict"; //С трогий режим
+alert ('Hello, world!'); // Вивід повідомлення
+let Vlad; // Задання змінної
+Vlad = 'Nasadyk'; // Збереження змінної
+alert(Vlad); // Вивід змінної
+let Olya = 'Dyak'; // Спрощення двох дій з змінними
+const myBirthday = '06.09.2020'; // Створення константи
+let admin;
+let name;
+name = 'Джон';
+admin = name;
+alert(admin);
