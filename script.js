61d5357aef91e854feec92c9c3299b24a94741a6
Nasadyk Vladyslav <t.empire228@gmail.com>
2020-03-27 11:27:20 +0200

    3. Ink/Dek/Prompt

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-"use strict"; //С трогий режим
+"use strict"; // Строгий режим
 alert ('Hello, world!'); // Вивід повідомлення
 let Vlad; // Задання змінної
 Vlad = 'Nasadyk'; // Збереження змінної
 alert(Vlad); // Вивід змінної
 let Olya = 'Dyak'; // Спрощення двох дій з змінними
 const myBirthday = '06.09.2020'; // Створення константи
-let admin;
-let name;
-name = 'Джон';
-admin = name;
-alert(admin);
+
+let vlad = 18;
+vlad++; // Інкремент/Декремент
+alert(vlad); 
+
+result = prompt ('Скільки тобі років?'); // Прийом інформації від користувача (prompt)
+alert ('Мені ${result} років!');
+
