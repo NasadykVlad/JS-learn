6f8ee5acf5e8775f1cc218e44cd3b7470119a2bd
Nasadyk Vladyslav <t.empire228@gmail.com>
2020-05-13 12:25:21 +0300

    15. JQuery: Selectors, Events, Effects, man. with DOM

--- jquery_functions.js
+++ jquery_functions.js
@@ -1,5 +1,94 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    $(".Ruslan").hide();
-  });
+/*
+
+
+
+$(document).ready(function () {
+$(".Ruslan").click(function () { 
+	$(this).hide(); // Сховати
+	console.log("Все пахає!")
 });
+});
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+$("button").click(function () {
+	$("div").slideDown(); // Слайд вниз
+	console.log("Все пахає!");
+});
+});
+
+
+
+
+
+
+
+$(document).ready(function () {
+$("button").click(function () {
+	console.log("Text: " + $("p").text("")); // Маніпуляція з ДОМ (GET in DOM)
+});
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+$("button").click(function () {
+	$("p").text("NE VlAD, a SOFIA!"); // SET in DOM
+});
+});
+
+
+
+
+
+
+$(document).ready(function () {
+$("button").click(function () {
+	$("p").append(" Sofiyka!"); // ADD in DOM
+});
+});
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+$("button").click(function () {
+	$("p").remove(); // REMOVE IN DOM
+});
+});
+
+*/
+
+
+
+
+$(document).ready(function () {
+$("button").click(function () {
+	$("html").css("background-color", "yellow");
+});
+});
+
+
+
+
+
+
+
+
+
